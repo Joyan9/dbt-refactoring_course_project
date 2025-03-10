23fc9063b390fcb0a0af6324798dd5eef4baad85
Joyan9 <joyansbhathena@gmail.com>
2025-03-10 18:42:58 +0100

    replaced hard-coded table references with sources

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -6,20 +6,20 @@ WITH paid_orders as (select Orders.ID as order_id,
     p.payment_finalized_date,
     C.FIRST_NAME    as customer_first_name,
         C.LAST_NAME as customer_last_name
-FROM bvg-open-data-project.jaffle_shop.orders as Orders
+FROM {{ref('stg__orders')}} as Orders
 left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-        from bvg-open-data-project.jaffle_shop.stripe_payments
+        from {{ref('stg__payments')}}
         where STATUS <> 'fail'
         group by 1) p ON orders.ID = p.order_id
-left join bvg-open-data-project.jaffle_shop.customer C on orders.USER_ID = C.ID ),
+left join {{ref('stg__customers')}} C on orders.USER_ID = C.ID ),
 
 customer_orders 
 as (select C.ID as customer_id
     , min(ORDER_DATE) as first_order_date
     , max(ORDER_DATE) as most_recent_order_date
     , count(ORDERS.ID) AS number_of_orders
-from bvg-open-data-project.jaffle_shop.customer C 
-left join bvg-open-data-project.jaffle_shop.orders as Orders
+from {{ref('stg__customers')}} C 
+left join {{ref('stg__orders')}} as Orders
 on orders.USER_ID = C.ID 
 group by 1)
 
